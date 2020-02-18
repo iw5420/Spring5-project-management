6a60a0d15a35ffe2655a70c146b61ec6ab224069
yienwanghome <iw5420@gmail.com>
2020-02-18 11:56:43 +0800

    firstcommit

---
+++ src/main/java/com/jrp/pma/entities/Project.java
@@ -0,0 +1,57 @@
+package com.jrp.pma.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Project {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long projectId;
+	
+	private String nmae;
+	
+	private String stage; // NOTSTARTED, COMPLETED, INPROGRESS
+	
+	private String description;
+	
+	public Project() {
+		
+	}
+	
+	public Project(String nmae, String stage, String description) {
+		super();
+		this.nmae = nmae;
+		this.stage = stage;
+		this.description = description;
+	}
+	
+	public long getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(long projectId) {
+		this.projectId = projectId;
+	}
+	public String getNmae() {
+		return nmae;
+	}
+	public void setNmae(String nmae) {
+		this.nmae = nmae;
+	}
+	public String getStage() {
+		return stage;
+	}
+	public void setStage(String stage) {
+		this.stage = stage;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
