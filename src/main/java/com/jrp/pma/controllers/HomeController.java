8c2a4c2dde746555959a472bba04d9bad8e3d70c
yienwanghome <iw5420@gmail.com>
2020-02-24 16:01:03 +0800

    2020/02/24 chartjs套用, 並用objectMapper轉物件為json字串去做傳遞

--- src/main/java/com/jrp/pma/controllers/HomeController.java
+++ src/main/java/com/jrp/pma/controllers/HomeController.java
@@ -1,15 +1,20 @@
 package com.jrp.pma.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jrp.pma.dao.EmployeeRepository;
 import com.jrp.pma.dao.ProjectRepository;
-import com.jrp.pma.entities.Employee;
+import com.jrp.pma.dto.ChartData;
+import com.jrp.pma.dto.EmployeeProject;
 import com.jrp.pma.entities.Project;
 
 @Controller
@@ -22,15 +27,22 @@ public class HomeController {
 	EmployeeRepository empRepo;
 
 	@GetMapping("/")
-	public String displayHome(Model model) {
+	public String displayHome(Model model) throws JsonProcessingException {
 		
 		// we are querying the database for projects
 		List<Project> projects = proRepo.findAll();
 		model.addAttribute("projectsList",projects);
 		
+		List<ChartData>projectData = proRepo.getProjectStatus();
+		//Lets convert projectData object into a json structure for use in javascript
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(projectData);
+		//[["NOTTARTED", 1], ["INPROGRESS", 2], ["COMPLETED", 1]]
+		
+		model.addAttribute("projectStatusCnt", jsonString);
 		// we are querying the database for employees
-		List<Employee> employees = empRepo.findAll();
-		model.addAttribute("employeeList",employees);
+		List<EmployeeProject> employeesProjectCnt = empRepo.employeeProjects();
+		model.addAttribute("employeeListProjectsCnt",employeesProjectCnt);
 		return "main/home";
 	}
 }
